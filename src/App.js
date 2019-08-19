ad9d784ab27672829e82d03100cb02c46429802c
Saad <saadmasood96@hotmail.com>
2019-08-19 18:12:18 -0400

    create form input comp, login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 import Header from './components/Header/Header';
 import { Switch, Route } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/login" component={LoginPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import SignIn from '../../components/SignIn/SignIn';
+
+import './LoginPage.scss';
+
+const LoginPage = () => <SignIn />;
+
+export default LoginPage;
