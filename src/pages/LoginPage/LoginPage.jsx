6bc9f0bc8add6a86d375e153cb9f042e53d3912c
Saad <saadmasood96@hotmail.com>
2019-08-20 16:48:48 -0400

    add sign up comp to login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
 
 import './LoginPage.scss';
 
-const LoginPage = () => <SignIn />;
+const LoginPage = () => (
+  <div className="login">
+    <SignIn />
+    <SignUp />
+  </div>
+);
 
 export default LoginPage;
