f844a9340d912359b34f5d5fe876930b640239d2
Saad <saadmasood96@hotmail.com>
2019-08-20 16:31:27 -0400

    store user data in firestore on sign in

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -32,7 +32,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     } catch (err) {
       console.log('error creating user', err.message);
     }
-
     return userRef;
   }
 };
