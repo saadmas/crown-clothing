8d603f0970d0f17ea7a1be82897998224176ce06
Saad <saadmasood96@hotmail.com>
2019-08-24 20:15:01 -0400

    add and remove item from cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,3 +9,19 @@ export const addItemToCart = (cartItems, itemToAdd) => {
 
   return [...cartItems, { ...itemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, itemToRemove) => {
+  const itemInCart = cartItems.find(item => item.id === itemToRemove.id);
+
+  if (itemInCart && itemInCart.quantity === 1) {
+    return cartItems.filter(item => item.id !== itemToRemove.id);
+  } else if (itemInCart) {
+    return cartItems.map(item =>
+      item.id === itemToRemove.id
+        ? { ...item, quantity: item.quantity - 1 }
+        : item
+    );
+  }
+
+  return [...cartItems];
+};
